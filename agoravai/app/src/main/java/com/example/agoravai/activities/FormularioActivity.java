cf48697d7339963193a5afe1ebf716438c39443b
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-04-27 21:55:17 -0300

    projeto crud

---
+++ agoravai/app/src/main/java/com/example/agoravai/activities/FormularioActivity.java
@@ -0,0 +1,99 @@
+package com.example.agoravai.activities;
+
+import java.io.File;
+import java.io.Serializable;
+
+import com.example.agoravai.R;
+import com.example.agoravai.dao.AlunoDAO;
+import com.example.agoravai.helpers.FormularioHelper;
+import com.example.agoravai.model.Aluno;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class FormularioActivity extends Activity {
+
+    private FormularioHelper helper;
+    private String localArquivoFoto;
+    private static final int TIRA_FOTO = 123;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formulario);
+
+        Intent intent = getIntent();
+        final Aluno alunoParaSerAlterado = (Aluno) intent.getSerializableExtra("alunoSelecionado");
+
+        this.helper = new FormularioHelper(this);
+
+        Button botao = (Button) findViewById(R.id.botao);
+
+        if (alunoParaSerAlterado != null) {
+            botao.setText("Alterar");
+            helper.colocaAlunoNoFormulario(alunoParaSerAlterado);
+        }
+
+        botao.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Aluno aluno = helper.pegaAlunoDoFormulario();
+
+                AlunoDAO dao = new AlunoDAO(FormularioActivity.this);
+
+                if (alunoParaSerAlterado == null) {
+                    dao.insere(aluno);
+                } else {
+                    aluno.setId(alunoParaSerAlterado.getId());
+                    dao.altera(aluno);
+                }
+
+                dao.close();
+
+                finish();
+            }
+        });
+
+        ImageView foto = helper.getBotaoImagem();
+        foto.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                localArquivoFoto = Environment.getExternalStorageDirectory() +
+                        "/"+ System.currentTimeMillis()+".jpg";
+
+                File arquivo = new File(localArquivoFoto);
+                Uri localFoto = Uri.fromFile(arquivo);
+
+                Intent irParaCamera =
+                        new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                irParaCamera.putExtra(MediaStore.EXTRA_OUTPUT, localFoto);
+                startActivityForResult(irParaCamera, TIRA_FOTO);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == TIRA_FOTO) {
+            if (resultCode == Activity.RESULT_OK) {
+                helper.carregaImagem(this.localArquivoFoto);
+            } else {
+                this.localArquivoFoto = null;
+            }
+        }
+    }
+}
+
