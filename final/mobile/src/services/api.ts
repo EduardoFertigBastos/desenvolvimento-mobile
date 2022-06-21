0df189e8ca96ab760134b2e047ef36eb7d7f3fa3
Eduardo Fertig Bastos <69549838+EduardoFertigBastos@users.noreply.github.com>
2022-06-20 21:56:21 -0300

    react native

--- final/mobile/src/services/api.ts
+++ final/mobile/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Platform } from 'react-native';
 
 const api = axios.create({
-    baseURL: Platform.OS === "ios" ? 'http://localhost:3333' : 'http://10.0.2.2:3333',
+    baseURL: 'http://192.168.0.114:3333',
 });
 
 export default api;
